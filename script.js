9ef6a80239cc126dda5d2de6c3a96e4afcd99d2d
Iman Heydari <s240380@th-ab.de>
2026-05-24 17:53:55 +0200

    upgrade cursor style

--- script.js
+++ script.js
@@ -39,3 +39,19 @@ slideLeftBtn.addEventListener('click', () => {
 slideRightBtn.addEventListener('click', () => {
     teamSlider.scrollBy({ left: 400, behavior: 'smooth' });
 });
+
+// Custom Cursor Implementation
+const innerCursor = document.querySelector('.inner-cursor');
+const outerCursor = document.querySelector('.outer-cursor');
+
+document.addEventListener('mousemove', moveCursor);
+
+function moveCursor(e) {
+    let x = e.clientX;
+    let y = e.clientY;
+
+    innerCursor.style.left = `${x}px`;
+    innerCursor.style.top = `${y}px`;
+    outerCursor.style.left = `${x}px`;
+    outerCursor.style.top = `${y}px`;
+}
